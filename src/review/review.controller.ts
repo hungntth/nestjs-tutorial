05a995f17ff9ba56df27cdae252a83f24271141d
HungNt <hungnt@techasians.com>
2023-10-07 11:57:40 +0700

    api create product

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,15 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { CurrentUserDecorator } from 'src/utility/decorators/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewService.create(createReviewDto);
+  create(
+    @Body() createReviewDto: CreateReviewDto,
+    @CurrentUserDecorator() currentUser: UserEntity,
+  ) {
+    return this.reviewService.create(createReviewDto, currentUser);
   }
 
   @Get()
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewEntity } from './entities/review.entity';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReviewEntity]), ProductsModule],
   controllers: [ReviewController],
   providers: [ReviewService]
 })
